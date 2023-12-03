30229d5a097e19ac5d80d519696f446b40a0c3fb
MahammadAqib <126152672+MahammadAqib@users.noreply.github.com>
2023-12-03 17:08:13 +0530

    Update penguins_app.py

--- penguins_app.py
+++ penguins_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Penguin Prediction App
